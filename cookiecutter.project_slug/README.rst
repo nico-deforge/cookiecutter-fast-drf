00230d7d934b08b09a30f0d5bbea42c4c637d3e6
Nicolas Deforge <me@nicolasdeforge.com>
2021-05-21 09:48:46 +0200

    Delete Django secret key

---
+++ cookiecutter.project_slug/README.rst
@@ -0,0 +1,3 @@
+Setup your project :
+1. Uncomment vscode folders in gitignore file
+2. Replace Django SECRET KEY
--- cookiecutter.project_slug/src/cookiecutter_fast_drf/settings.py
+++ cookiecutter.project_slug/src/cookiecutter_fast_drf/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'eey#t2w^!3g)b)b3o)n74vw+xz+(6%e!0kc5+%33s829^qkgqe'
+SECRET_KEY = 'Your project secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
