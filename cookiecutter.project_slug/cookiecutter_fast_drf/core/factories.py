ee7c151d17b5013cff0e15391a1b5447acdca209
Nicolas Deforge <me@nicolasdeforge.com>
2021-05-23 20:21:35 +0200

    pass UserFactory test

--- cookiecutter.project_slug/cookiecutter_fast_drf/core/factories.py
+++ cookiecutter.project_slug/cookiecutter_fast_drf/core/factories.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class UserFactory(factory.DjangoModelFactory):
+class UserFactory(factory.django.DjangoModelFactory):
     email = factory.Sequence(lambda n: f"nicolas{n}@example.com")
     password = factory.PostGenerationMethodCall("set_password", "Testable44")
 
