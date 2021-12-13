52cf6ba978d8b2f3df4b4d91744bce36e4e0f425
Nicolas Deforge <me@nicolasdeforge.io>
2021-12-13 09:42:29 +0100

    add dates in uuid model

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/core/models.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/core/models.py
@@ -13,6 +13,9 @@ from django.contrib.auth.hashers import make_password
 class UUIDmodel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
     class Meta:
         abstract = True
 
